e0ed4f96512d65ddd2d94acbe1c45b77508c7501
Paul Kim <paulkim0516@gmail.com>
2017-07-27 14:05:30 +0900

    hide soft keyboard and clear password on attemptLogin()

--- app/src/main/java/com/hwuiwon/alma/LoginActivity.java
+++ app/src/main/java/com/hwuiwon/alma/LoginActivity.java
@@ -3,6 +3,7 @@ package com.hwuiwon.alma;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -62,7 +64,9 @@ public class LoginActivity extends AppCompatActivity {
         passwordET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                if (id == R.id.login || id == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(textView.getApplicationWindowToken(),0);
                     attemptLogin();
                     return true;
                 }
@@ -82,6 +86,8 @@ public class LoginActivity extends AppCompatActivity {
                     loginPrefsEditor.clear();
                     loginPrefsEditor.commit();
                 }
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getApplicationWindowToken(),0);
                 attemptLogin();
             }
         });
@@ -96,6 +102,7 @@ public class LoginActivity extends AppCompatActivity {
         passwordET.setError(null);
         String username = usernameET.getText().toString();
         String password = passwordET.getText().toString();
+        passwordET.setText("");
 
         boolean cancel = false;
         View focusView = null;
