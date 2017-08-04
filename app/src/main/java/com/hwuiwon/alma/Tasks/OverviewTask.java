14f348b202409e9cfb963de8372ceadb2b27664c
Paul Kim <paulkim0516@gmail.com>
2017-08-04 17:54:53 +0900

    Fixed OverviewTask
    Calculates time differences between current date and min/max dates of date picker to decide whether to select previous day or next day for overviews.

--- app/src/main/java/com/hwuiwon/alma/Tasks/OverviewTask.java
+++ app/src/main/java/com/hwuiwon/alma/Tasks/OverviewTask.java
@@ -12,6 +12,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +25,8 @@ public class OverviewTask extends AsyncTask<String, Void, Overview[]> {
 
     private int tmp = 0;
     private Overview[] overviews = null;
+    public int NAVIGATE_FRONT = 0;
+    public int NAVIGATE_BACK = 1;
 
     @Override
     protected Overview[] doInBackground(String... strings) {
@@ -27,6 +35,8 @@ public class OverviewTask extends AsyncTask<String, Void, Overview[]> {
         String url = "https://spps.getalma.com/";
 
         String date;
+        String responseString = "Student is not enrolled in any classes";
+        Connection.Response response;
 
         try {
             Document document = Jsoup.connect(url+"home").timeout(0)
@@ -40,32 +50,47 @@ public class OverviewTask extends AsyncTask<String, Void, Overview[]> {
             String studentID = m.group(1);
 
             date = document.select(".date-picker > a").get(0).attr("data-date");
-            Connection.Response response =
-                    Jsoup.connect(url+"home/get-student-schedule?studentId="+studentID+"&date="+date)
-                            .header("Cookie", cookie).ignoreContentType(true)
-                            .method(Connection.Method.GET).execute();
 
-            String responseString = escapeUnicode(response.body().split("\"html\":\"")[1].split("\"\\}")[0]);
+            String strDateMin = document.select(".date-picker > input").get(0).attr("min");
+            String strDateMax = document.select(".date-picker > input").get(0).attr("max");
+            String strCurrentDate = document.select(".date-picker > input").get(0).attr("value");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            Date dateMin = sdf.parse(strDateMin);
+            Date dateMax = sdf.parse(strDateMax);
+            Date currentDate = sdf.parse(strCurrentDate);
+            int navigateMode =
+                    Math.abs(dateMin.getTime()-currentDate.getTime())>Math.abs(dateMax.getTime()-currentDate.getTime())?
+                            NAVIGATE_FRONT:NAVIGATE_BACK;
+
+            while (responseString.contains("Student is not enrolled in any classes")||responseString.contains("No classes scheduled today.")) {
+                response =
+                        Jsoup.connect(url+"home/get-student-schedule?studentId="+studentID+"&date="+date)
+                                .header("Cookie", cookie).ignoreContentType(true)
+                                .method(Connection.Method.GET).execute();
+
+                responseString = escapeUnicode(response.body().split("\"html\":\"")[1].split("\"\\}")[0]);
+                Log.d("Debug", responseString);
+                date = Jsoup.parse(responseString).select(".date-picker > a").get(1).attr("data-date");
+            }
+
 
             overviews = new Overview[Jsoup.parse(responseString).select("tr").size()];
 
             setOverviews(Jsoup.parse(responseString).select("tbody").get(0).select("tr"));
 
-            date = Jsoup.parse(responseString).select(".date-picker > a").get(0).attr("data-date");
-
             String responseString2 = responseString;
-            while(responseString.equals(responseString2)) {
+            while(responseString.equals(responseString2)||responseString.contains("No classes scheduled today.")) {
                 response =
                         Jsoup.connect(url + "home/get-student-schedule?studentId=" + studentID + "&date=" + date)
                                 .header("Cookie", cookie).ignoreContentType(true)
                                 .method(Connection.Method.GET).execute();
                 responseString2 = escapeUnicode(response.body().split("\"html\":\"")[1].split("\"\\}")[0]);
-                date = Jsoup.parse(responseString2).select(".date-picker > a").get(0).attr("data-date");
+                date = Jsoup.parse(responseString2).select(".date-picker > a").get(1).attr("data-date");
             }
 
             setOverviews(Jsoup.parse(responseString2).select("tbody").get(0).select("tr"));
 
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
 
